cfda9c5596941ed0c4e8ba6990f532a1184ef15d
sip-23 <ndiramandindi@gmail.com>
2025-07-27 05:05:19 +0200

    Added the HTML implementation and attempted Javascript
    Output is not correct

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-const initialTasks = [
+// This is the given array to be added on
+
+let initialTasks = [
   {
     id: 1,
     title: "Launch Epic Career",
@@ -21,3 +23,53 @@ const initialTasks = [
 ];
 
 // Continue from here ... 
+
+// Creating function that will get input and validate
+function GetTask(initialTasks) {
+
+  // Prompt user for the number of tasks they must add.
+  const taskcount = prompt("How many Task would you like to add today (Must be beween 3 and 6)?")
+
+  for (let i = 1; i < taskcount + 1 ; i++) {
+
+    // Prompting for the task title
+    let taskTitle = prompt(`Please enter Task: ${i} title`)
+    while (!taskTitle || taskTitle.trim() === "") {
+      taskTitle = prompt(`Task: ${i} title cannot be empty. Please enter a value for Task: ${i} title`)
+    }
+    taskTitle = taskTitle.trim()
+
+    // prompting for the task description
+    let taskDescrip = prompt(`Please enter Task: ${i} description`)
+    while (!taskDescrip || taskDescrip.trim() === "") {
+      taskDescrip = prompt(`Task: ${i} description cannot be empty. Please enter a value for Task: ${i} description`)
+    }
+    taskDescrip = taskDescrip.trim()
+
+    let taskStatus;
+    do {
+      taskStatus = prompt(`Enter Task ${i} status (todo/doing/done):`);
+      if (taskStatus) {
+        taskStatus = taskStatus.toLowerCase();
+      }
+      if (taskStatus !== "todo" && taskStatus !== "doing" && taskStatus !== "done"){
+        alert("INVALID STATUS! Please enter only: todo, doing, or done");
+      }
+    } while (taskStatus !== "todo" && taskStatus !== "doing" && taskStatus !== "done");
+
+    let output = {
+      id: initialTasks.length + 1,
+      title: taskTitle.trim(),
+      description: taskDescrip.trim() ,
+      status: taskStatus};
+
+    output = initialTasks.push(output);
+
+    return output;
+  }
+}
+
+// Call the function and 
+initialTasks.push(GetTask(initialTasks));
+
+console.log(initialTasks)
