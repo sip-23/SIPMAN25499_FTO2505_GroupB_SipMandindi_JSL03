0fb7ce43e597e169f7ed7b218f40624d1d4419bb
sip-23 <ndiramandindi@gmail.com>
2025-07-27 06:14:53 +0200

    Output is not correct. P2.21 - P2.25 have been implemented successfully and ensured ; are added
    Work on console logs for desired outputs

--- scripts.js
+++ scripts.js
@@ -28,23 +28,32 @@ let initialTasks = [
 function GetTask(initialTasks) {
 
   // Prompt user for the number of tasks they must add.
-  const taskcount = prompt("How many Task would you like to add today (Must be beween 3 and 6)?")
+  let taskCount; 
+  do {
+    taskCount = parseInt(prompt("How many Tasks would you like to add today (Must be beween 1 and 3)?"));
+    if (isNaN(taskCount)) {
+      alert("Please enter a valid number beween 1 and 3!");
+    }
+    else if (taskCount < 1 || taskCount > 3) {
+      alert("The value entered is not between 1 and 3! Ensure your value is valid number beween 1 and 3!");
+    }
+  } while (isNaN(taskCount) || taskCount < 1 || taskCount > 3 );
 
-  for (let i = 1; i < taskcount + 1 ; i++) {
+  for (let i = 1; i <= taskCount ; i++) {
 
     // Prompting for the task title
-    let taskTitle = prompt(`Please enter Task: ${i} title`)
+    let taskTitle = prompt(`Please enter Task: ${i} title`);
     while (!taskTitle || taskTitle.trim() === "") {
-      taskTitle = prompt(`Task: ${i} title cannot be empty. Please enter a value for Task: ${i} title`)
+      taskTitle = prompt(`Task: ${i} title cannot be empty. Please enter a value for Task: ${i} title`);
     }
-    taskTitle = taskTitle.trim()
+    taskTitle = taskTitle.trim();
 
     // prompting for the task description
-    let taskDescrip = prompt(`Please enter Task: ${i} description`)
+    let taskDescrip = prompt(`Please enter Task: ${i} description`);
     while (!taskDescrip || taskDescrip.trim() === "") {
-      taskDescrip = prompt(`Task: ${i} description cannot be empty. Please enter a value for Task: ${i} description`)
+      taskDescrip = prompt(`Task: ${i} description cannot be empty. Please enter a value for Task: ${i} description`);
     }
-    taskDescrip = taskDescrip.trim()
+    taskDescrip = taskDescrip.trim();
 
     let taskStatus;
     do {
@@ -57,19 +66,31 @@ function GetTask(initialTasks) {
       }
     } while (taskStatus !== "todo" && taskStatus !== "doing" && taskStatus !== "done");
 
-    let output = {
+    // P2.24 implement here: Storing task details in object
+    const output = {
       id: initialTasks.length + 1,
       title: taskTitle.trim(),
       description: taskDescrip.trim() ,
-      status: taskStatus};
+      status: taskStatus
+    };
 
-    output = initialTasks.push(output);
-
-    return output;
+    // Add Looped task into the the exhisting array
+    initialTasks.push(output);
+    console.log(`Task: ${output.id} has been successfully added.`);
   }
+
+  // Alert when when the loop has ended 
+  alert(`Tasks saved! You have logged ${taskCount} tasks. There are now ${initialTasks.length}. Check the console for details`);
+
+  // Implement user stroy P2.25
+  if (initialTasks.length >= 6) {
+    alert(`You have reached the limit of ${initialTasks.length} tasks! There are enough tasks on your board, please check them in the console`);
+  }
+
+  return initialTasks;
 }
 
 // Call the function and 
-initialTasks.push(GetTask(initialTasks));
+initialTasks = GetTask(initialTasks);
 
-console.log(initialTasks)
+console.log(initialTasks);
